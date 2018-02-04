31c614dcbf8006b9782c0875f6a6cc5f6e7598b9
Ammar <amrmrdb1@gmail.com>
2018-02-04 08:52:44 +0300

    v2.3.4

--- src/lib/types/hook.ts
+++
@@ -1,7 +0,0 @@
-export interface IHook {
-  isHook: boolean;
-  name: string;
-  private: boolean;
-}
-
-export type IHookOptions = string | { name: string, args?: any }
--- src/lib/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './api-types';
-export * from './hook';
-export * from './response-codes';
-export * from './response-domains';
-export * from './response-error';
-export * from './response';
-export * from './service';
