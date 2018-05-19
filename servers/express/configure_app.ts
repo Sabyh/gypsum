3f0c604ecc69b521763fe0abd49ee95bc6bc866d
AmmAr MouRad <amrmrd111spcl@hotmail.com>
2018-05-19 13:36:00 +0300

    v2.6.39

--- servers/express/configure_app.ts
+++ servers/express/configure_app.ts
@@ -9,22 +9,22 @@ import { Logger } from '../../misc/logger';
 import { App } from '../../app';
 
 export function configure(expressApp: express.Express, app?: App, logger?: Logger) {
-  logger = logger || new Logger(app ? app.name : 'express');  
+  logger = logger || new Logger(app ? app.name : 'express');
 
   if (State.env === 'production')
     expressApp.use(compress());
 
-  if (app) {
-    expressApp.use((req, res, next) => {
-      logger!.info(`request: ${req.method} - ${req.originalUrl}`);
-      if ('onRequest' in app)
-        (<any>app).onRequest(req, res, next);
-      else
-        next();
-    });
+  expressApp.use((req, res, next) => {
+    logger!.info(`request: ${req.method} - ${req.originalUrl}`);
+    if (app && 'onRequest' in app)
+      (<any>app).onRequest(req, res, next);
+    else
+      next();
+  });
 
+  if (app) {
     logger.info('impliminting express middlewares..');
-      
+
     expressApp.use(cookieParser(State.config.cookie_key));
     expressApp.use(bodyParser.urlencoded({ extended: true }));
     expressApp.use(bodyParser.json({ limit: `${State.config.upload_size_limit_mb}mb` }));
